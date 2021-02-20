6754093d14bb46b865b6e8029efa4993820dbc0e
Christopher Kalla <christopherkalla@christophersmbp.lan>
2021-02-20 12:06:51 -0700

    adding comments to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { ReactiveBase, CategorySearch } from '@appbaseio/reactivesearch';
+import { ReactiveBase, CategorySearch} from '@appbaseio/reactivesearch';
 
 class App extends Component {
     render() {
         return (
         <ReactiveBase
+        //Configuring connection to Elastic Search
         app="band-name-search"
         credentials="fBHGxspkl:638536db-9420-4528-b99a-a40c4a0ea2b3"
         >
             <CategorySearch
+            //Simple search box for querying band names
             componentId="searchbox"
             dataField="bandName"
             categoryField="bandName.keyword"
